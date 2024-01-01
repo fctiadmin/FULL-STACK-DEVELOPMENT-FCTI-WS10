d4bed233105e64a3ecd0e712fc0397cf07d8b877
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2024-01-01 15:52:54 +0600

    Add files via upload

---
+++ JS/CLASS04/apps.js
@@ -0,0 +1,24 @@
+// var mark=22;
+// document.write(mark<33? 'F' : 'Pass');//this is ternary operator
+
+// var mark=22; // this is logic
+// if(mark<33){
+//     document.write('F');
+// }else{
+//     document.write('Pass');
+// }
+
+
+//for loop
+ //i=1;
+// for(var i=2;i<=20;i=i+2){
+//     document.write(i+'<br>');
+// }
+
+
+//while loop
+var i=1;
+while(i<=20){
+    document.write(i+'<br>');
+    i=i+2;
+}
